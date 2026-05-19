1ee2678fd8b3cc8746949640dd60971ab4591361
Sami Daniel <samidanielpersonal@gmail.com>
2026-05-18 22:58:08 -0300

    Add author information to App.java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+// Nome: Sami Daniel Santos Silva
+// Matricula: 1261950200
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
